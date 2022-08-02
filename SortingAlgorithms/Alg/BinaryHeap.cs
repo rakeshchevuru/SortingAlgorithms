3f3bc01ce59f43a493fc3bbb5f61a8ca96804392
Rakesh <rakeshchevuru@users.noreply.github.com>
2022-08-02 17:38:39 -0600

    Add quick sort & max heap

---
+++ SortingAlgorithms/Alg/BinaryHeap.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*  Construct Binary heap from an array
+ *  
+ *  Binary heap is a complete binary tree
+ *  
+ *  For every index, we need to find left and right child
+ *  
+ *  root = index = 0
+ *  left child = 2*i + 1
+ *  right child = 2*i + 2
+ * 
+ * 
+ */
+
+namespace SortingAlgorithms.Alg
+{
+    internal class BinaryHeap
+    {
+        public int[] Arr { get; set; }
+
+        public BinaryHeap(int[] input)
+        {
+            Arr = input;
+        }
+
+        /*  Max Heap property should start from the bottom most subtree
+         * 
+         *  Take the bottom most (starting from right) and check for MaxHeap (parent should be greater than children and max should be at the root)
+         *  
+         *  Swap nodes if MaxHeap is not satisfied.
+         *  Once swapped, re-check the maxheap property on the swapped node
+         * 
+         * ex: arr = [10, 20, 40, 30, 80, 60, 50]
+         * size = 6 (last index of the array)
+         * 
+         *          10
+         *         /   \
+         *       20     40
+         *      /  \   /  \
+         *    30   80 60  50
+         */
+
+
+        public void BuildHeap(int[] arr, int size)
+        {
+            int i;
+
+            for (i = size / 2; i >=0; i--)
+            {
+                MaxHeapify(arr, i, size);
+            }
+        }
+
+        private void MaxHeapify(int[] arr, int index, int size)
+        {
+            var left = 2 * index + 1;
+            var right = left + 1;
+
+            int max = index;
+
+            if (left <= size && arr[left] > arr[max])
+                max = left;
+
+            if (right <= size && arr[right] > arr[max])
+                max = right;
+
+            if(index != max)
+            {
+                Helpers.SwapWithTuple(arr, max, index);
+
+                MaxHeapify(arr, max, size);
+            }
+        }
+    }
+}
---
+++ SortingAlgorithms/Sort/04QuickSort.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms.Sort
+{
+    internal class QuickSort
+    {
+        public int[] Input { get; set; }
+
+        public QuickSort(int[] input)
+        {
+            Input = input;
+        }
+
+        public void Sort()
+        {
+            QuickSrt(Input, 0, Input.Length - 1);
+        }
+
+        public void QuickSrt(int[] arr, int start, int end)
+        {
+            if(start < end)
+            {
+                var pIndex = Partion(arr, start, end);
+                QuickSrt(arr, start, pIndex - 1);
+                QuickSrt(arr, pIndex + 1, end);
+            }
+        }
+
+        public int Partion(int[] arr, int start, int end)
+        {
+            var pIndex = start;
+            var pivot = arr[end];
+            int i;
+
+            for (i = start; i < end; i++)
+            {
+                if (arr[i] < pivot)
+                {
+                    Helpers.SwapWithTuple(arr, i, pIndex);
+                    pIndex++;
+                }
+            }
+
+            Helpers.SwapWithTuple(arr, end, pIndex);
+
+            return pIndex;
+        }
+
+    }
+}
